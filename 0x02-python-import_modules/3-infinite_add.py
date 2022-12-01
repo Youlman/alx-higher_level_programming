e1bcbcefd01f9227e29492e38977a34ffac3918e
Youlman <youlmankamal@gmail.com>
2022-12-01 10:53:29 +0000

    Addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of arguments."""
+    import sys
+
+    length = len(sys.argv) - 1
+    result = 0
+
+    for i in range(length):
+        result += int(sys.argv[i + 1])
+
+    print(result)
+
