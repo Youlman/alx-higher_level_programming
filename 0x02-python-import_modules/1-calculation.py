3f3cae57e0868807aa6c341580f1b326bf8f09d6
Youlman <youlmankamal@gmail.com>
2022-12-01 03:57:34 -0800

    Fix errors

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     """Print the sum, difference, the product, the divisin of 10 and 5."""
     from calculator_1 import add, sub, mul, div
-    
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,12 +3,8 @@
 if __name__ == "__main__":
     """Print the number of arguments."""
     import sys
-
     length = len(sys.argv) - 1
     result = 0
-
     for i in range(length):
         result += int(sys.argv[i + 1])
-
     print(result)
-
