8228779fd1a63b36e988ceef33e9e74c548405ad
Youlman <youlmankamal@gmail.com>
2022-12-04 02:14:07 -0800

    Repace an element of list without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,12 +9,12 @@ def new_in_list(my_list, idx, element):
         Returns:
             copy original list or new list
     """
-    new_list = []
-    for i in range(len(my_list)):
-        new_list.append(my_list[i])
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
-    if idx < 0 or idx > (len(new_list) - 1):
-        return (new_list)
+    new_list = []
+    for val in my_list:
+        new_list.append(val)
 
     new_list[idx] = element
     return (new_list)
