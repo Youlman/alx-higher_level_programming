d0e1311e73151913c6741ed8c2ca9f86568c247a
Youlman <youlmankamal@gmail.com>
2022-12-05 04:49:28 -0800

    Deletes the item at a specific poisition

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -10,8 +10,8 @@ def delete_at(my_list=[], idx=0):
     """
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    
-    for i in range(idx,len(my_list)):
+
+    for i in range(idx, len(my_list)):
         if i == len(my_list) - 1:
             del my_list[i]
             break
