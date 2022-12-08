8ff8abf9f973cf8269494c27cf746795e400ee03
Youlman <youlmankamal@gmail.com>
2022-12-08 01:17:00 -0800

    Common elments in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """ common elements in two sets
+        Args:
+            set_1: The first set
+            set_2: the second set
+        Returns:
+            set of common elements
+    """
+
+    return (set_1.intersection(set_2))
