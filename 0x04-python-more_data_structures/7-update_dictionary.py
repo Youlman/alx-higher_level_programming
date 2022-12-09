7ef9355c61e37353650f67a95a0b73a10eef6552
Youlman <youlmankamal@gmail.com>
2022-12-09 04:58:37 -0800

    add exo

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary.i
+        Args:
+            a_dictionary: a dictionary
+            key: key
+            value: value to add
+    """
+    a_dictionary[key] = value
+    return (a_dictionary)
