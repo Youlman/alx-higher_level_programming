970420be4604adfaa8672cc3df31d7432e0aee0e
Youlman <youlmankamal@gmail.com>
2022-12-20 02:32:45 -0800

    Initialize a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Define a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize the size"""
+        self.__size = size
