0c5a832282a5f39ff76c14a3977c4548e18d40e8
Youlman <youlmankamal@gmail.com>
2023-01-03 14:41:19 -0800

    text indentation

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" print_square prints a square depending on the "size" parameter
+"""
+
+
+def print_square(size):
+    """ Prints a square with a size
+        Args:
+            size: an integer the size of the square
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        return None
+
+    for row in range(size):
+        print('#' * size)
