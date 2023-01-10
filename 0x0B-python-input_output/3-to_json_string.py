020df1086f7da310f76f9c068f09b486c931170f
Youlman <youlmankamal@gmail.com>
2023-01-10 18:13:17 +0000

    JSON representation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """ JSON representation of an object """
-    if isinstance(my_obj, set):
-        raise TypeError(f"{my_obj} is not JSON serializable")
     return (json.dumps(my_obj))
