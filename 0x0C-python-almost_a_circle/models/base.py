00e19c36c66c5d6ff10e437356bfda6f28cdb32c
Youlman <youlmankamal@gmail.com>
2023-01-16 03:29:48 -0800

    writes the JSON string representation of list_objs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,11 +24,25 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of list dictionaries 
+        """ returns the JSON string representation of list dictionaries
             Args:
-                list_dictionaries(list): A list of dictionaries.                
+                list_dictionaries(list): A list of dictionaries.
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write an object to a text file using JSON representation.
+            Args:
+                list_objs(list): list of Base
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [ob.to_dictionary() for ob in list_objs]
+                f.write(Base.to_json_string(list_dicts))
