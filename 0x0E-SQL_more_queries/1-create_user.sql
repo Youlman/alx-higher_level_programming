a8f9d8a1425e32aa5a0409bfb136cf455d5bf5b6
Youlman <youlmankamal@gmail.com>
2023-02-15 10:25:16 +0000

    More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,9 @@
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXITS 'sammy'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 
+	IF NOT EXITS 'user_0d_1'@'localhost' 
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+	ON *.* 
+	TO 'user_0d_1'@'localhost' 
+	WITH GRANT OPTION;
+FLUSH PRIVILEGES;
