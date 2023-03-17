acf85fdf8d4a8c2dbe95e9d6cefa17348a1973a0
Youlman <youlmankamal@gmail.com>
2023-03-17 17:44:23 +0000

    List all states start N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM `states`ORDER BY  `id`")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     conn.close()
