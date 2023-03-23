e7d8f9728857cce1e6697eb59b5f6c769d8a38e0
Youlman <youlmankamal@gmail.com>
2023-03-23 12:43:09 +0000

    Fix errors

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,7 +4,7 @@ from the database"""
 
 import sys
 from relationship_city import City 
-from relationship_state import State
+from relationship_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    session.add(City(name="San Francisco", state=State(name="California")))
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    c = City(name="San Francisco")
+    s = State(name="California", cities=[c])
+    session.add(s)
+    session.add(c)
     session.commit()
