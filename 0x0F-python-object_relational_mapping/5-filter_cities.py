e47c5435d9bf7cb214c3d7b4bda78f4d7db0c54d
Youlman <youlmankamal@gmail.com>
2023-03-23 11:51:08 +0000

    FIx errors

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,10 +3,11 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-        user=sys.argv[1], 
-        passwd=sys.argv[2],                         
+        user=sys.argv[1],
+        passwd=sys.argv[2],
         db=sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM cities AS c \
