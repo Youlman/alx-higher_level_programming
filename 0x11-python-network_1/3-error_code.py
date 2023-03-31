00d3ddd5270468ef63cdb88d00a3005a7f5cbe50
Youlman <youlmankamal@gmail.com>
2023-03-31 22:23:54 +0000

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Sends a request to the URL and displays
+the body of the response"""
+
+import sys
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = Request(url)
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        print('Error code: ', e.code)
