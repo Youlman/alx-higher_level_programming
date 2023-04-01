5a522af4cb2d98235f6c937ad24facb46f61ddb6
Youlman <youlmankamal@gmail.com>
2023-04-01 00:03:39 +0000

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status
+using package requests """
+
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print(req.headers["X-Request-Id"])
+    
