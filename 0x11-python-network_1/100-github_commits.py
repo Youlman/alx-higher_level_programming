2a841653cd61e3379012d99b2c856c2391a038f6
Youlman <youlmankamal@gmail.com>
2023-04-02 20:40:59 +0000

    get 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,12 @@ import requests
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
-    req = requests.get(url).json()
-    for i in range(10):
-        print("{}: {}".format(
-            req[i].get("sha"),
-            req[i].get("commit").get("author").get("name")))
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
