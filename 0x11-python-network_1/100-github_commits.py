eb7c8d19d29811b2680d5c578d3909623a66a209
Youlman <youlmankamal@gmail.com>
2023-04-02 20:35:45 +0000

    get 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Print all commits by: `<sha>: <author name>` using Github API"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2])
+    req = requests.get(url).json()
+    for i in range(10):
+        print("{}: {}".format(
+            req[i].get("sha"), 
+            req[i].get("commit").get("author").get("name")))
